1265d8a342f6c5b79f65e1446544d182b18e7520
Vu-Hoai-Nam2001 <huuphuchp06@gmail.com>
2023-05-08 23:42:26 +0700

    đổ dữ liệu

--- src/components/home/PLVS.jsx
+++ src/components/home/PLVS.jsx
@@ -32,12 +32,12 @@ export default function Index(props) {
         console.log("gọi lại api")
         const callApi = async () => {
 
-            await fetch(`https://qlvbcc.hasura.app/api/rest/get_tensv/${props.data}`, {
+            await fetch(`${import.meta.env.VITE_ABOUT_STUDENT}${props.data}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${await getToken({
-                        template: `hasura-qlvbcc`
+                        template: import.meta.env.VITE_TEMPLATE_EDU_QLVBCC
                     })}`,
                 },
                 // body:JSON.stringify({masv:'1912101003'})
@@ -45,7 +45,7 @@ export default function Index(props) {
             })
                 .then(response => response.json())
                 .then(datasv => {
-                    setDatasv(datasv.f_test_sinhvien)
+                    setDatasv(datasv.f_get_ttsv)
 
                 });
         }
@@ -55,85 +55,192 @@ export default function Index(props) {
 
     return (
         <div>
-            <h3>masinhvien {datasv && datasv.map((sinhvien, index) => (
+            {datasv && datasv.map((sinhvien, index) => (
                 <div key={index}>
-                    <p>Mã sinh viên: {sinhvien.masinhvien}</p>
-                    <p>Họ đệm: {sinhvien.hodem}</p>
-                    <p>Tên: {sinhvien.ten}</p>
-                    <p>Mã lớp: {sinhvien.malop}</p>
-                    <p>Nơi sinh: {sinhvien.noisinh}</p>
-                </div>
-            ))}</h3>
-            <div className="flex justify-between mt-[30px]">
-                <div className="flex">
-                    <img
-                        src={logo}
-                        alt="logo"
-                        className="object-cover  left-0 h-[75px]"
-                    />
-                    <div className="flex flex-col justify-center items-center ">
-                        <a className="font-bold ">BỘ GIÁO DỤC VÀ ĐÀO TẠO </a>
-                        <a className="font-bold underline">TRƯỜNG ĐẠI HỌC QUẢN LÝ VÀ CÔNG NGHỆ HẢI PHÒNG</a>
+                    <div className="flex justify-between mt-[30px]">
+                        <div className="flex">
+                            <img
+                                src={logo}
+                                alt="logo"
+                                className="object-cover  left-0 h-[75px]"
+                            />
+                            <div className="flex flex-col justify-center items-center ">
+                                <a className="font-bold ">BỘ GIÁO DỤC VÀ ĐÀO TẠO </a>
+                                <a className="font-bold underline">TRƯỜNG ĐẠI HỌC QUẢN LÝ VÀ CÔNG NGHỆ HẢI PHÒNG</a>
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-center ">
+                            <a className="font-bold ">CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM</a>
+                            <a className="font-bold underline">Độc lập - Tự do - Hạnh phúc</a>
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-center items-center mt-[50px]">
+                        <h2 className="font-bold text-[30px] ">PHỤ LỤC VĂN BẰNG</h2>
+                    </div>
+                    <div className="flex  ml-[30px]  w-[100%] mt-[50px] ">
+                        <div className="flex flex-col w-[50%] gap-[5px]">
+                            <a>Họ và tên: {sinhvien.hoten}</a>
+
+                            <a>Giới tính: {sinhvien.goitinh}</a>
+
+                            <a>Mã sinh viên: {sinhvien.masinhvien}</a>
+
+                            <a>Số CCCD: {sinhvien.socmnd}</a>
+
+                            <a>Hình thức đào tạo: {sinhvien.hedaotao}</a>
+
+                            <a>Trình độ đào tạo: ..........</a>
+                        </div>
+                        <div className="flex flex-col w-[50%] gap-[5px]">
+                            <a>Ngôn ngữ đào tạo: ...................</a>
+
+                            <a>Ngày sinh: {sinhvien.nganysinh.split(` `)[0].split('-').reverse().join('-')}</a>
+
+                            <a>Khóa: {sinhvien.tenkhoahoc}</a>
+
+                            <a>Chuyên ngành: {sinhvien.tennganh}</a>
+
+                            <a>Ngày nhập học: {sinhvien.ngayvaotruong}</a>
+
+                            <a>Thời gian đào tạo: {sinhvien.thoigiandaotao}</a>
+                        </div>
+                    </div>
+                    <Table />
+                    <div className="flex justify-between mt-[30px] ">
+                        <div className="flex flex-col gap-[5px]">
+                            <a >Tên đề tài tốt nghiệp: <span className="font-bold">{data.tendetaitotnghiep}</span></a>
+
+                            <a >Điểm trung bình toàn khóa(hệ 4):<span className="font-bold"> {data.DTBTK}</span></a>
+
+                            <a >Tổng số tín chỉ: <span className="font-bold">{data.TSTC}</span></a>
+
+                            <a >Xếp hạng tốt nghiệp: <span className="font-bold">{sinhvien.xeploaitotnghiep}</span> </a>
+
+                            <a>Số hiệu văn bằng: <span className="font-bold">{sinhvien.sohieubang}</span> </a>
+
+                            <a >Số vào sổ ngày cấp văn bằng: <span className="font-bold">{sinhvien.sovaoso}</span> </a>
+                        </div>
+
+                        <div className="flex flex-col justify-center  items-center">
+                            <a className="">Hải Phòng,ngày....tháng....năm....</a>
+                            <a className="">TL, HIỆU TRƯỞNG</a>
+                            <a>TRƯỞNG PHÒNG ĐÀO TẠO - QLKH</a>
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center ">
-                    <a className="font-bold ">CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM</a>
-                    <a className="font-bold underline">Độc lập - Tự do - Hạnh phúc</a>
-                </div>
-            </div>
-            <div className="flex flex-col justify-center items-center mt-[50px]">
-                <h2 className="font-bold text-[30px] ">PHỤ LỤC VĂN BẰNG</h2>
-            </div>
-            <div className="flex  ml-[30px]  w-[100%] mt-[50px] ">
-                <div className="flex flex-col w-[50%] gap-[5px]">
-                    <a>Họ và tên:{datasv.masinhvien}</a>
+            ))}
 
-                    <a>Giới tính: {data.sex}</a>
-
-                    <a>Mã sinh viên: {data.MSV}</a>
-
-                    <a>Số CCCD: {data.SoCCCD}</a>
-
-                    <a>Hình thức đào tạo: {data.Hinhthucdaotao}</a>
-
-                    <a>Trình độ đào tạo: {data.TRinhDoDT}</a>
-                </div>
-                <div className="flex flex-col w-[50%] gap-[5px]">
-                    <a>Ngôn ngữ đào tạo: {data.ngonngu}</a>
-
-                    <a>Ngày sinh: {data.ngaysinh}</a>
-
-                    <a>Khóa: {data.khoa}</a>
-
-                    <a>Chuyên ngành: {data.chuyennganh}</a>
-
-                    <a>Ngày nhập học: {data.ngaynhaphoc}</a>
-
-                    <a>Thời gian đào tạo: {data.thoigiandaotao}</a>
-                </div>
-            </div>
-            <Table />
-            <div className="flex justify-between mt-[30px] ">
-                <div className="flex flex-col gap-[5px]">
-                    <a >Tên đề tài tốt nghiệp: <span className="font-bold">{data.tendetaitotnghiep}</span></a>
-
-                    <a >Điểm trung bình toàn khóa(hệ 4):<span className="font-bold"> {data.DTBTK}</span></a>
-
-                    <a >Tổng số tín chỉ: <span className="font-bold">{data.TSTC}</span></a>
-
-                    <a >Xếp hạng tốt nghiệp: <span className="font-bold">{data.XHTN}</span> </a>
-
-                    <a>Số hiệu văn bằng: <span className="font-bold">{data.SHVB}</span> </a>
-
-                    <a >Số vào sổ ngày cấp văn bằng: <span className="font-bold">{data.SVSNCB}</span> </a>
-                </div>
-
-                <div className="flex flex-col justify-center  items-center">
-                    <a className="">Hải Phòng,ngày....tháng....năm....</a>
-                    <a className="">TL, HIỆU TRƯỞNG</a>
-                    <a>TRƯỞNG PHÒNG ĐÀO TẠO - QLKH</a>
-                </div>
-            </div>
         </div>
     )
 }
+
+
+
+
+// export default function Index(props) {
+//     console.log(props)
+//     const [datasv, setDatasv] = useState([])
+
+//     const { getToken } = useAuth();
+//     useEffect(() => {
+//         console.log("gọi lại api")
+//         const callApi = async () => {
+
+//             await fetch(`${import.meta.env.VITE_EDU_ALL_SCORE}1912111003`, {
+//                 method: 'GET',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'Authorization': `Bearer ${await getToken({
+//                         template: import.meta.env.VITE_TEMPLATE_EDU_QLVBCC
+//                     })}`,
+//                 },
+//                 // body:JSON.stringify({masv:'1912101003'})
+
+//             })
+//                 .then(response => response.json())
+//                 .then(res => setDatasv(res));
+//         }
+//         callApi();
+//     });
+//     console.log(datasv)
+
+//     return (
+//         <div>
+//             {datasv && datasv.map((sinhvien, index) => (
+//                 <div key={index}>
+//                     <div className="flex justify-between mt-[30px]">
+//                         <div className="flex">
+//                             <img
+//                                 src={logo}
+//                                 alt="logo"
+//                                 className="object-cover  left-0 h-[75px]"
+//                             />
+//                             <div className="flex flex-col justify-center items-center ">
+//                                 <a className="font-bold ">BỘ GIÁO DỤC VÀ ĐÀO TẠO </a>
+//                                 <a className="font-bold underline">TRƯỜNG ĐẠI HỌC QUẢN LÝ VÀ CÔNG NGHỆ HẢI PHÒNG</a>
+//                             </div>
+//                         </div>
+//                         <div className="flex flex-col justify-center items-center ">
+//                             <a className="font-bold ">CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM</a>
+//                             <a className="font-bold underline">Độc lập - Tự do - Hạnh phúc</a>
+//                         </div>
+//                     </div>
+//                     <div className="flex flex-col justify-center items-center mt-[50px]">
+//                         <h2 className="font-bold text-[30px] ">PHỤ LỤC VĂN BẰNG</h2>
+//                     </div>
+//                     <div className="flex  ml-[30px]  w-[100%] mt-[50px] ">
+//                         <div className="flex flex-col w-[50%] gap-[5px]">
+//                             <a>Họ và tên: {sinhvien.hoten}</a>
+
+//                             <a>Giới tính: {sinhvien.goitinh}</a>
+
+//                             <a>Mã sinh viên: {sinhvien.masinhvien}</a>
+
+//                             <a>Số CCCD: {sinhvien.socmnd}</a>
+
+//                             <a>Hình thức đào tạo: {sinhvien.hedaotao}</a>
+
+//                             <a>Trình độ đào tạo: ..........</a>
+//                         </div>
+//                         <div className="flex flex-col w-[50%] gap-[5px]">
+//                             <a>Ngôn ngữ đào tạo: ...................</a>
+
+//                             <a>Ngày sinh: {sinhvien.nganysinh.split(` `)[0].split('-').reverse().join('-')}</a>
+
+//                             <a>Khóa: {sinhvien.tenkhoahoc}</a>
+
+//                             <a>Chuyên ngành: {sinhvien.tennganh}</a>
+
+//                             <a>Ngày nhập học: {sinhvien.ngayvaotruong}</a>
+
+//                             <a>Thời gian đào tạo: {sinhvien.thoigiandaotao}</a>
+//                         </div>
+//                     </div>
+//                     <Table />
+//                     <div className="flex justify-between mt-[30px] ">
+//                         <div className="flex flex-col gap-[5px]">
+//                             <a >Tên đề tài tốt nghiệp: <span className="font-bold">{data.tendetaitotnghiep}</span></a>
+
+//                             <a >Điểm trung bình toàn khóa(hệ 4):<span className="font-bold"> {data.DTBTK}</span></a>
+
+//                             <a >Tổng số tín chỉ: <span className="font-bold">{data.TSTC}</span></a>
+
+//                             <a >Xếp hạng tốt nghiệp: <span className="font-bold">{sinhvien.xeploaitotnghiep}</span> </a>
+
+//                             <a>Số hiệu văn bằng: <span className="font-bold">{sinhvien.sohieubang}</span> </a>
+
+//                             <a >Số vào sổ ngày cấp văn bằng: <span className="font-bold">{sinhvien.sovaoso}</span> </a>
+//                         </div>
+
+//                         <div className="flex flex-col justify-center  items-center">
+//                             <a className="">Hải Phòng,ngày....tháng....năm....</a>
+//                             <a className="">TL, HIỆU TRƯỞNG</a>
+//                             <a>TRƯỞNG PHÒNG ĐÀO TẠO - QLKH</a>
+//                         </div>
+//                     </div>
+//                 </div>
+//             ))}
+
+//         </div>
+//     )
+// }
