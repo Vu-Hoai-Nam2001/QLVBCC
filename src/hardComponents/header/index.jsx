e536177aaef612c79ae230667c263e2603b53c51
Vu-Hoai-Nam2001 <huuphuchp06@gmail.com>
2023-06-16 10:22:47 +0700

    hoàng ngu

--- src/hardComponents/header/index.jsx
+++ src/hardComponents/header/index.jsx
@@ -38,10 +38,12 @@ export default function Index() {
             </svg>
           </button>
           {isOpen && (
-            <ul className="dropdown-content absolute bg-sky-600  text-white pt-1 w-[170px]">
-              <li><a className="rounded-t bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/edit">Chỉnh sửa</a></li>
+            <ul className="dropdown-content absolute bg-sky-600  text-white pt-1 w-[190px]">
+              <li><a className="rounded-t bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/edit">Cấp phát phụ lục văn bằng</a></li>
               <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/teacher">Tìm kiếm</a></li>
-              <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/soluuvanbangchungchi">Sổ lưu phụ lục văn bằng chứng chỉ</a></li>
+              <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/bosungvanbang">Bổ sung minh chứng văn bằng</a></li>
+              <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/bosungchungchi">Bổ sung minh chứng chứng chỉ</a></li>
+              <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/soluuvanbangchungchi">Cập nhật sổ sưu</a></li>
               <li><a className="rounded-b bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" onClick={() => signOut()}>Đăng xuất</a></li>
             </ul>
           )}
