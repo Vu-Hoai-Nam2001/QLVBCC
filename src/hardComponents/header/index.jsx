1eb04c9d7a3bb195f2978c4b79dd7a2d051aad75
Vu-Hoai-Nam2001 <huuphuchp06@gmail.com>
2023-06-17 07:35:41 +0700

    hoai nam

--- src/hardComponents/header/index.jsx
+++ src/hardComponents/header/index.jsx
@@ -43,7 +43,7 @@ export default function Index() {
               <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/teacher">Tìm kiếm</a></li>
               <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/bosungvanbang">Bổ sung minh chứng văn bằng</a></li>
               <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/bosungchungchi">Bổ sung minh chứng chứng chỉ</a></li>
-              <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/soluuvanbangchungchi">Cập nhật sổ sưu</a></li>
+              <li><a className="bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="/soluuvanbangchungchi">Tra cứu văn bằng </a></li>
               <li><a className="rounded-b bg-sky-600  hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" onClick={() => signOut()}>Đăng xuất</a></li>
             </ul>
           )}
